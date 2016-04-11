6b9b6535c54e9fd902f779a87a3605e953a9e890
Joshua Aresty <joshua.aresty@emc.com>
2016-04-11 08:49:56 -0400

    Use config file to run rexray insteal of env
    - run raxray in foreground to make sure it could be kill by test correctly
    - format tabs to spaces
    
    [#117003441](https://www.pivotaltracker.com/story/show/117003441)
    
    Signed-off-by: Xuebin He <xuebin.he@emc.com>

--- acceptance/acceptance_suite_test.go
+++ acceptance/acceptance_suite_test.go
@@ -1,17 +1,17 @@
 package acceptance_test
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-	"strings"
-	"testing"
-	"time"
+"fmt"
+"os"
+"os/exec"
+"strings"
+"testing"
+"time"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gexec"
-	"github.com/tedsuo/ifrit/ginkgomon"
+. "github.com/onsi/ginkgo"
+. "github.com/onsi/gomega"
+"github.com/onsi/gomega/gexec"
+"github.com/tedsuo/ifrit/ginkgomon"
 )
 
 var volmanPath string
@@ -29,78 +29,74 @@ var keyringFileContents string
 var clusterIp string
 
 func TestVolman(t *testing.T) {
-	// these integration tests can take a bit, especially under load;
-	// 1 second is too harsh
-	SetDefaultEventuallyTimeout(10 * time.Second)
+    // these integration tests can take a bit, especially under load;
+    // 1 second is too harsh
+  SetDefaultEventuallyTimeout(10 * time.Second)
 
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "RexRay Driver Cmd Suite")
+  RegisterFailHandler(Fail)
+  RunSpecs(t, "RexRay Driver Cmd Suite")
 }
 
 var _ = SynchronizedBeforeSuite(func() []byte {
-	var err error
-	volmanPath, err = gexec.Build("github.com/cloudfoundry-incubator/volman/cmd/volman", "-race")
-	Expect(err).NotTo(HaveOccurred())
+  var err error
+  volmanPath, err = gexec.Build("github.com/cloudfoundry-incubator/volman/cmd/volman", "-race")
+  Expect(err).NotTo(HaveOccurred())
 
-	driverPath = "/home/ubuntu/funnyrexray" //todo: inject via ENV varible
+    driverPath = "/home/ubuntu/funnyrexray" //todo: inject via ENV varible
 
-	return []byte(strings.Join([]string{volmanPath, driverPath}, ","))
-}, func(pathsByte []byte) {
-	path := string(pathsByte)
-	volmanPath = strings.Split(path, ",")[0]
-	driverPath = strings.Split(path, ",")[1]
+    return []byte(strings.Join([]string{volmanPath, driverPath}, ","))
+  }, func(pathsByte []byte) {
+    path := string(pathsByte)
+    volmanPath = strings.Split(path, ",")[0]
+    driverPath = strings.Split(path, ",")[1]
 
-	// read config files
-	// keyringFileContents = os.Getenv("rexray_KEYRING")
-	// clusterIp = os.Getenv("rexray_CLUSTER_IP")
-})
+    // read config files
+    // keyringFileContents = os.Getenv("rexray_KEYRING")
+    // clusterIp = os.Getenv("rexray_CLUSTER_IP")
+    })
 
 var _ = BeforeEach(func() {
-	var err error
-	tmpDriversPath = "/etc/docker/plugins/"
-	Expect(err).NotTo(HaveOccurred())
+  var err error
+  tmpDriversPath = "/etc/docker/plugins/"
+  Expect(err).NotTo(HaveOccurred())
 
-	commandWithEnv := exec.Command(driverPath)
-	currentEnv := os.Environ()
-	newEnv := append(currentEnv, "SCALEIO_PROTECTIONDOMAINNAME=default")
-	newEnv = append(newEnv, "SCALEIO_USERID=fc5118f500000000")
-	newEnv = append(newEnv, "SCALEIO_STORAGEPOOLNAME=default")
-	newEnv = append(newEnv, "SCALEIO_SYSTEMNAME=")
-	newEnv = append(newEnv, "SCALEIO_PROTECTIONDOMAINID=4c9e0aa100000000")
-	newEnv = append(newEnv, "SCALEIO_THINORTHICK=ThinProvisioned")
-	newEnv = append(newEnv, "SCALEIO_SYSTEMID=5c4c45f02c98551c")
-	newEnv = append(newEnv, "REXRAY_STORAGEDRIVERS=scaleio")
-	newEnv = append(newEnv, "SCALEIO_PASSWORD=Some1Example")
-	newEnv = append(newEnv, "SCALEIO_USERNAME=admin")
-	newEnv = append(newEnv, "SCALEIO_USECERTS=true")
-	newEnv = append(newEnv, "SCALEIO_ENDPOINT=https://52.87.123.252/api")
-	newEnv = append(newEnv, "SCALEIO_STORAGEPOOLID=8b3b0c5300000000")
-	newEnv = append(newEnv, "SCALEIO_INSECURE=true")
-	newEnv = append(newEnv, "SCALEIO_VERSION=2.0")
-	commandWithEnv.Env = newEnv
+  commandWithEnv := exec.Command(
+    driverPath, 
+    "start",
+    "-f",
+    )
 
-	unixDriverRunner = ginkgomon.New(ginkgomon.Config{
-		Name:       "RexRayServer",
-		Command:    commandWithEnv,
-		StartCheck: "started",
-	})
+  driverServerPort = 7979
 
-	volmanServerPort = 8750 + GinkgoParallelNode()
-	debugServerAddress = fmt.Sprintf("0.0.0.0:%d", 8850+GinkgoParallelNode())
-	volmanRunner = ginkgomon.New(ginkgomon.Config{
-		Name: "volman",
-		Command: exec.Command(
-			volmanPath,
-			"-listenAddr", fmt.Sprintf("0.0.0.0:%d", volmanServerPort),
-			"-debugAddr", debugServerAddress,
-			"-driversPath", tmpDriversPath,
-		),
-		StartCheck: "volman.started",
-	})
-})
+  commandWithEnv.Env = os.Environ()
+
+  unixDriverRunner = ginkgomon.New(ginkgomon.Config{
+    Name:       "RexRayServer",
+    Command:    commandWithEnv,
+    StartCheck: "",
+    })
+
+  volmanServerPort = 8750 + GinkgoParallelNode()
+  debugServerAddress = fmt.Sprintf("0.0.0.0:%d", 8850+GinkgoParallelNode())
+  volmanRunner = ginkgomon.New(ginkgomon.Config{
+    Name: "volman",
+    Command: exec.Command(
+      volmanPath,
+      "-listenAddr", fmt.Sprintf("0.0.0.0:%d", volmanServerPort),
+      "-debugAddr", debugServerAddress,
+      "-driversPath", tmpDriversPath,
+      ),
+    StartCheck: "volman.started",
+    })
+  })
+
+var _ = AfterEach(func() {
+  time.Sleep(time.Millisecond * 1000)
+  os.RemoveAll(tmpDriversPath)
+  })
 
 var _ = SynchronizedAfterSuite(func() {
 
 }, func() {
-	gexec.CleanupBuildArtifacts()
-})
+  gexec.CleanupBuildArtifacts()
+  })
--- acceptance/acceptance_test.go
+++ acceptance/acceptance_test.go
@@ -1,27 +1,27 @@
 package acceptance_test
 
 import (
-	"github.com/cloudfoundry-incubator/volman/certification"
-	"github.com/nu7hatch/gouuid"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	"github.com/tedsuo/ifrit/ginkgomon"
+"github.com/cloudfoundry-incubator/volman/certification"
+"github.com/nu7hatch/gouuid"
+. "github.com/onsi/ginkgo"
+. "github.com/onsi/gomega"
+"github.com/tedsuo/ifrit/ginkgomon"
 )
 
 var _ = Describe("Rexray Driver Certification", func() {
-	certification.CertifiyWith("Rexraydriver", func() (*ginkgomon.Runner, *ginkgomon.Runner, int, string, string, int, string, func() (string, map[string]interface{})) {
+  certification.CertifiyWith("Rexraydriver", func() (*ginkgomon.Runner, *ginkgomon.Runner, int, string, string, int, string, func() (string, map[string]interface{})) {
 
-		uuid, err := uuid.NewV4()
-		Expect(err).NotTo(HaveOccurred())
-		volumeName := "rexray-volume-name_" + uuid.String()
-		volumeId := "rexray-volume-id_" + uuid.String()
+    uuid, err := uuid.NewV4()
+    Expect(err).NotTo(HaveOccurred())
+    volumeName := "rexray-volume-name_" + uuid.String()
+    volumeId := "rexray-volume-id_" + uuid.String()
 
-		volumeInfo := func() (string, map[string]interface{}) {
-			localMountPoint := tmpDriversPath + "/_rexraydriver-" + volumeId
-			opts := map[string]interface{}{"keyring": keyringFileContents, "ip": clusterIp, "localMountPoint": localMountPoint, "remoteMountPoint": "unused"}
-			return volumeName, opts
-		}
+    volumeInfo := func() (string, map[string]interface{}) {
+      localMountPoint := tmpDriversPath + "/_rexray-" + volumeId
+      opts := map[string]interface{}{"keyring": keyringFileContents, "ip": clusterIp, "localMountPoint": localMountPoint, "remoteMountPoint": "unused"}
+      return volumeName, opts
+    }
 
-		return unixDriverRunner, volmanRunner, volmanServerPort, debugServerAddress, tmpDriversPath, driverServerPort, "rexraydriver", volumeInfo
-	})
-})
+    return unixDriverRunner, volmanRunner, volmanServerPort, debugServerAddress, tmpDriversPath, driverServerPort, "rexray", volumeInfo
+    })
+  })
